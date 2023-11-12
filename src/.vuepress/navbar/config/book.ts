1a684bf2e6443ee2221409f4e4fc74999b49096d
yuanyxh <15766118362@139.com>
2023-11-13 03:00:13 +0800

    feat: add a uniapp_multi_end-adaptation_problem posts

--- src/.vuepress/navbar/config/book.ts
+++ src/.vuepress/navbar/config/book.ts
@@ -3,7 +3,8 @@ export default [
     text: '前端',
     prefix: 'web/',
     children: [
-      { text: 'JavaScript 高级程序设计', link: 'JavaScript 高级程序设计' }
+      { text: 'JavaScript 高级程序设计', link: 'JavaScript 高级程序设计' },
+      { text: '你不知道的 JavaScript', link: "you_don't_know_js" }
     ]
   }
 ];
