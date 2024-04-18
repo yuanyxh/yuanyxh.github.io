4ed7dd0aa633769db1172bae18e6eac78cbe25c4
yuanyxh <15766118362@139.com>
2024-04-18 15:02:58 +0800

    feat: complete books pae

--- src/viewer/Books.tsx
+++ src/viewer/Books.tsx
@@ -1,12 +1,49 @@
+import { Typography } from 'antd';
+
+import { Link, type RouteObject } from '@/router';
+
 import { useBooks } from './hooks/useBooks';
+import styles from './styles/Books.module.less';
+
+const { Title, Paragraph } = Typography;
+
+const BookItem = ({ book }: { book: RouteObject }) => {
+  return (
+    <Link to={`/books/${book.path}`}>
+      <div className={styles.bookItem}>
+        <div className={styles.contentBox}>
+          <Title className={styles.title} level={3}>
+            {book.meta?.title}
+          </Title>
+
+          <Paragraph
+            type="secondary"
+            ellipsis={{ rows: 5, tooltip: book.meta?.description }}
+            style={{ marginTop: 18, fontSize: 16 }}
+          >
+            {book.meta?.description}
+          </Paragraph>
+        </div>
+
+        <div className={styles.imageBox}>
+          <img
+            loading="lazy"
+            src={book.meta?.imageUrl}
+            alt={book.meta?.title}
+          />
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 const Books = () => {
   const books = useBooks();
 
   return (
-    <div>
+    <div className={styles.books}>
       {books.map((book) => (
-        <div key={book.fullPath}>{book.meta?.title}</div>
+        <BookItem key={book.fullPath} book={book} />
       ))}
     </div>
   );
